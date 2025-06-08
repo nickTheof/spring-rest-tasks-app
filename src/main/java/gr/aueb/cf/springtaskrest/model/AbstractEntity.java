2222c1c6eeff5ce3c0b935bfadc515e4109ce188
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-08 13:58:33 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/springtaskrest/model/AbstractEntity.java
@@ -0,0 +1,34 @@
+package gr.aueb.cf.springtaskrest.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity implements Serializable {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false, name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/test/java/gr/aueb/cf/springtaskrest/SpringTaskRestApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.springtaskrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaskRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
