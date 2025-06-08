2b401b9121a0ea618700a5adc5d192289cb6a633
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-08 14:15:06 +0300

    feat: add repository interfaces for domain models

---
+++ src/main/java/gr/aueb/cf/springtaskrest/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.springtaskrest.repository;
+
+import gr.aueb.cf.springtaskrest.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+    Optional<Task> findByUuid(String uuid);
+    Page<Task> findByUserUuid(String uuid, Pageable pageable);
+}
---
+++ src/main/java/gr/aueb/cf/springtaskrest/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.springtaskrest.repository;
+
+import gr.aueb.cf.springtaskrest.core.enums.Role;
+import gr.aueb.cf.springtaskrest.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> , JpaSpecificationExecutor<User> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByUuid(String uuid);
+    Page<User> findByRole(Role role, Pageable pageable);
+    Page<User> findByIsActive(Boolean isActive, Pageable pageable);
+}
