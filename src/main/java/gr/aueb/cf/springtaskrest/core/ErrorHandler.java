4e5154b7c5f165e08de96a661962bf4f95c62c04
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-09 22:30:16 +0300

    feat: add @ResponseStatus annotations to exception handlers for improved automated Swagger UI error response documentation

--- src/main/java/gr/aueb/cf/springtaskrest/core/ErrorHandler.java
+++ src/main/java/gr/aueb/cf/springtaskrest/core/ErrorHandler.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
@@ -18,6 +19,7 @@ import java.util.Map;
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ValidationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ApiErrorDTO> handleValidationException(ValidationException e, HttpServletRequest request) {
         BindingResult bindingResult = e.getBindingResult();
         Map<String, String> errors = new HashMap<>();
@@ -31,31 +33,37 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({AppObjectNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ApiErrorDTO> handleConstraintViolationException(AppObjectNotFoundException e, HttpServletRequest request) {
         return new ResponseEntity<>(new ApiErrorDTO(e.getCode(), e.getMessage(), System.currentTimeMillis(), request.getRequestURI()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({AppObjectAlreadyExistsException.class})
+    @ResponseStatus(HttpStatus.CONFLICT)
     public ResponseEntity<ApiErrorDTO> handleConstraintViolationException(AppObjectAlreadyExistsException e, HttpServletRequest request) {
         return new ResponseEntity<>(new ApiErrorDTO(e.getCode(), e.getMessage(), System.currentTimeMillis(), request.getRequestURI()), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler({AppObjectInvalidArgumentException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ApiErrorDTO> handleConstraintViolationException(AppObjectInvalidArgumentException e, HttpServletRequest request) {
         return new ResponseEntity<>(new ApiErrorDTO(e.getCode(), e.getMessage(), System.currentTimeMillis(), request.getRequestURI()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({AppObjectNotAuthorizedException.class})
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ResponseEntity<ApiErrorDTO> handleConstraintViolationException(AppObjectNotAuthorizedException e, HttpServletRequest request) {
         return new ResponseEntity<>(new ApiErrorDTO(e.getCode(), e.getMessage(), System.currentTimeMillis(), request.getRequestURI()), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler({AppServerException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ApiErrorDTO> handleConstraintViolationException(AppServerException e, HttpServletRequest request) {
         return new ResponseEntity<>(new ApiErrorDTO(e.getCode(), e.getMessage(), System.currentTimeMillis(), request.getRequestURI()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ApiErrorDTO> handleConstraintViolationException(Exception e, HttpServletRequest request) {
         return new ResponseEntity<>(new ApiErrorDTO("AppServerException", e.getMessage(), System.currentTimeMillis(), request.getRequestURI()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
