e4b92221d7d42ed96a1fb0159718d97ecff1f99d
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-09 23:02:48 +0300

    feat: create globally the swagger tags

--- src/main/java/gr/aueb/cf/springtaskrest/core/OpenApiConfig.java
+++ src/main/java/gr/aueb/cf/springtaskrest/core/OpenApiConfig.java
@@ -5,10 +5,18 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @OpenAPIDefinition(
         info = @Info(title = "Tasks Management API", version = "1.0"),
-        security = @SecurityRequirement(name = "bearerAuth")
+        security = @SecurityRequirement(name = "bearerAuth"),
+        tags = {
+                @Tag(name = "Authentication", description = "User authentication endpoints"),
+                @Tag(name = "Users", description = "User management endpoints"),
+                @Tag(name = "Tasks", description = "Tasks management endpoints"),
+                @Tag(name = "Admin", description = "Admin authentication endpoints"),
+                @Tag(name = "CurrentUser", description = "Current authenticated user endpoints")
+        }
 )
 @SecurityScheme(
         name = "bearerAuth",
