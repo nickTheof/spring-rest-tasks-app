06dbef2c19f1c433ccc99d32fb30f2e62abf9061
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-09 20:10:05 +0300

    refactor: split current authenticated user endpoints to separate controller

--- src/main/java/gr/aueb/cf/springtaskrest/rest/UserRestController.java
+++ src/main/java/gr/aueb/cf/springtaskrest/rest/UserRestController.java
@@ -8,7 +8,6 @@ import gr.aueb.cf.springtaskrest.dto.Paginated;
 import gr.aueb.cf.springtaskrest.dto.UserInsertDTO;
 import gr.aueb.cf.springtaskrest.dto.UserReadOnlyDTO;
 import gr.aueb.cf.springtaskrest.dto.UserUpdateDTO;
-import gr.aueb.cf.springtaskrest.model.User;
 import gr.aueb.cf.springtaskrest.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,19 +17,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserRestController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserRestController.class);
     private final UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping
     public ResponseEntity<Page<UserReadOnlyDTO>> getAllUsersPaginated(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size
@@ -39,7 +37,7 @@ public class UserRestController {
         return new ResponseEntity<>(usersPage, HttpStatus.OK);
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public ResponseEntity<UserReadOnlyDTO> insertUser(
             @Valid @RequestBody UserInsertDTO dto,
             BindingResult bindingResult) throws ValidationException, AppObjectAlreadyExistsException {
@@ -55,13 +53,13 @@ public class UserRestController {
         }
     }
 
-    @DeleteMapping("/users")
+    @DeleteMapping
     public ResponseEntity<Void> deleteAllUsers() {
         userService.deleteAllUsers();
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping("/users/filtered")
+    @PostMapping("/filtered")
     public ResponseEntity<Paginated<UserReadOnlyDTO>> getFilteredUsersPaginated(
             @Nullable @RequestBody UserFilters filters
             ) {
@@ -69,7 +67,7 @@ public class UserRestController {
         return new ResponseEntity<>(userService.getUsersFilteredPaginated(filters), HttpStatus.OK);
     }
 
-    @GetMapping("/users/{uuid}")
+    @GetMapping("/{uuid}")
     public ResponseEntity<UserReadOnlyDTO> getUserByUuid(
             @PathVariable("uuid") String uuid
     ) throws AppObjectNotFoundException {
@@ -82,7 +80,7 @@ public class UserRestController {
         }
     }
 
-    @PatchMapping("/users/{uuid}")
+    @PatchMapping("/{uuid}")
     public ResponseEntity<UserReadOnlyDTO> updateUser(
             @PathVariable("uuid") String uuid,
             @Valid @RequestBody UserUpdateDTO dto,
@@ -101,7 +99,7 @@ public class UserRestController {
         }
     }
 
-    @DeleteMapping("/users/{uuid}")
+    @DeleteMapping("/{uuid}")
     public ResponseEntity<Void> deleteUser(
             @PathVariable("uuid") String uuid
     ) throws  AppObjectNotFoundException {
@@ -114,47 +112,4 @@ public class UserRestController {
         }
     }
 
-    @GetMapping("/users/me")
-    public ResponseEntity<UserReadOnlyDTO> getMe(
-        @AuthenticationPrincipal User user
-    ) throws AppObjectNotFoundException {
-        try {
-            UserReadOnlyDTO readOnlyDTO = userService.findByUuid(user.getUuid());
-            return new ResponseEntity<>(readOnlyDTO, HttpStatus.OK);
-        } catch (AppObjectNotFoundException e) {
-            LOGGER.error("Getting user failed. {}", e.getMessage(), e);
-            throw e;
-        }
-    }
-
-    @PatchMapping("/users/me")
-    public ResponseEntity<UserReadOnlyDTO> updateMe(
-            @Valid @RequestBody UserUpdateDTO dto,
-            BindingResult bindingResult,
-            @AuthenticationPrincipal User user
-    ) throws ValidationException, AppObjectNotFoundException, AppObjectAlreadyExistsException {
-        if (bindingResult.hasErrors()) {
-            throw new ValidationException(bindingResult);
-        }
-        try {
-           UserReadOnlyDTO updatedUser = userService.updateUser(user.getUuid(), dto);
-           return new ResponseEntity<>(updatedUser, HttpStatus.OK);
-        } catch (AppObjectNotFoundException | AppObjectAlreadyExistsException e) {
-            LOGGER.error("Updating user failed. {}", e.getMessage(), e);
-            throw e;
-        }
-    }
-
-    @DeleteMapping("/users/me")
-    public ResponseEntity<Void> deleteMe(
-            @AuthenticationPrincipal User user
-            ) throws AppObjectNotFoundException {
-        try {
-            userService.reverseUserStatusActivity(user.getUuid());
-            return ResponseEntity.noContent().build();
-        } catch (AppObjectNotFoundException e) {
-            LOGGER.error("Deleting user failed. {}", e.getMessage(), e);
-            throw e;
-        }
-    }
 }
