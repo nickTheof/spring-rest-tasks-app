3845c2130eef063e51d1827f9e3abc5ba130a565
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-09 22:28:15 +0300

    feat: add schema details for enhance swagger-ui docs

--- src/main/java/gr/aueb/cf/springtaskrest/dto/ApiErrorDTO.java
+++ src/main/java/gr/aueb/cf/springtaskrest/dto/ApiErrorDTO.java
@@ -1,12 +1,20 @@
 package gr.aueb.cf.springtaskrest.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.Map;
 
+@Schema(description = "API Error details")
 public record ApiErrorDTO(
+        @Schema(description = "Application Error Code")
         String code,
+        @Schema(description = "Error message details")
         String message,
+        @Schema(description = "Field validation errors", nullable = true)
         Map<String, String> errors, // null if not a validation error
+        @Schema(description = "Timestamp in milliseconds since epoch")
         long timestamp,
+        @Schema(description = "Request path")
         String path // optional, can be null
 ) {
     public ApiErrorDTO(String code, String message, long timestamp, String path) {
