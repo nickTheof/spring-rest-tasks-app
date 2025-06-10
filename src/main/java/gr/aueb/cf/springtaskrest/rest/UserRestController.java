c64e5afb01b5162f4df9b94e92f10f77969e6bf1
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-10 09:10:29 +0300

    feat: use swagger annotations for enhancing swagger ui docs

--- src/main/java/gr/aueb/cf/springtaskrest/rest/UserRestController.java
+++ src/main/java/gr/aueb/cf/springtaskrest/rest/UserRestController.java
@@ -9,6 +9,12 @@ import gr.aueb.cf.springtaskrest.dto.UserInsertDTO;
 import gr.aueb.cf.springtaskrest.dto.UserReadOnlyDTO;
 import gr.aueb.cf.springtaskrest.dto.UserUpdateDTO;
 import gr.aueb.cf.springtaskrest.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -24,10 +30,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
+@Tags({
+        @Tag(name = "Admin"),
+        @Tag(name = "Users")
+})
 public class UserRestController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserRestController.class);
     private final UserService userService;
 
+    @Operation(
+            summary = "Get all users (paginated)",
+            description = "Returns a paginated list of all users. Only accessible by admin.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful retrieval of users"
+                    )
+            }
+    )
     @GetMapping
     public ResponseEntity<Page<UserReadOnlyDTO>> getAllUsersPaginated(
             @RequestParam(defaultValue = "0") int page,
@@ -37,6 +57,25 @@ public class UserRestController {
         return new ResponseEntity<>(usersPage, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Insert a new user",
+            description = "Admin creates a new user with selected role and status",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    content = @Content(
+                            schema = @Schema(implementation = UserInsertDTO.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "User created successfully",
+                            content = @Content(
+                                    schema = @Schema(implementation = UserReadOnlyDTO.class)
+                            )
+                    )
+            }
+    )
     @PostMapping
     public ResponseEntity<UserReadOnlyDTO> insertUser(
             @Valid @RequestBody UserInsertDTO dto,
@@ -53,12 +92,38 @@ public class UserRestController {
         }
     }
 
+    @Operation(
+            summary = "Delete all users",
+            description = "Admin deletes all users cascades to a delete to all tasks.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204", description = "Users successfully deleted"
+                    )
+            }
+    )
     @DeleteMapping
     public ResponseEntity<Void> deleteAllUsers() {
         userService.deleteAllUsers();
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+
+    @Operation(
+            summary = "Get filtered users (paginated)",
+            description = "Returns a paginated list of users matching provided filters. Only accessible by admin.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = false,
+                    content = @Content(
+                            schema = @Schema(implementation = UserFilters.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Filtered users retrieved successfully"
+                    )
+            }
+    )
     @PostMapping("/filtered")
     public ResponseEntity<Paginated<UserReadOnlyDTO>> getFilteredUsersPaginated(
             @Nullable @RequestBody UserFilters filters
@@ -67,6 +132,18 @@ public class UserRestController {
         return new ResponseEntity<>(userService.getUsersFilteredPaginated(filters), HttpStatus.OK);
     }
 
+
+    @Operation(
+            summary = "Get user by UUID",
+            description = "Retrieves a user by their unique UUID. Only accessible by admin.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User found",
+                            content = @Content(schema = @Schema(implementation = UserReadOnlyDTO.class))
+                    ),
+            }
+    )
     @GetMapping("/{uuid}")
     public ResponseEntity<UserReadOnlyDTO> getUserByUuid(
             @PathVariable("uuid") String uuid
@@ -80,6 +157,21 @@ public class UserRestController {
         }
     }
 
+    @Operation(
+            summary = "Update user by UUID",
+            description = "Updates a user with the given UUID. Only accessible by admin.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = UserUpdateDTO.class))
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User updated successfully",
+                            content = @Content(schema = @Schema(implementation = UserReadOnlyDTO.class))
+                    ),
+            }
+    )
     @PatchMapping("/{uuid}")
     public ResponseEntity<UserReadOnlyDTO> updateUser(
             @PathVariable("uuid") String uuid,
@@ -99,6 +191,17 @@ public class UserRestController {
         }
     }
 
+
+    @Operation(
+            summary = "Delete user by UUID",
+            description = "Deletes a user by their unique UUID. Only accessible by admin.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "User deleted successfully"
+                    ),
+            }
+    )
     @DeleteMapping("/{uuid}")
     public ResponseEntity<Void> deleteUser(
             @PathVariable("uuid") String uuid
